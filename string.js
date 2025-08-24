5db7de76bc443091d26a76456cf6f81b6e397432
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-25 05:23:05 +0600

    add the string js file

--- string.js
+++ string.js
@@ -1,6 +1,23 @@
 const conutry = 'Bangladesh';
 const division = "Khulna";
 const district = `Kushtia`;
-const thana = new String('mirpur');
+// const thana = new String('mirpur');
 
-console.log(typeof conutry);
+// console.log(thana)
+
+const numbers = [11 ,12, 13, 14, 15, 16];
+console.log(numbers);
+numbers[1] = 45;
+console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[2]);
+
+const capital = 'Daaka';
+// console.log(capital.length);
+// console.log(capital[1]);
+
+// string is immutable
+
+console.log(capital)
+capital[1] = 'h';
+console.log(capital);
