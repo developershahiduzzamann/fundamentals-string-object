2286a1691f74d744758e96fe91b3345961a87b30
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-30 21:44:47 +0600

    add the property js file

---
+++ property.js
@@ -0,0 +1,10 @@
+ const person = {
+    name: 'sabbir khan',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+ }
+//  console.log(person);
+// dot notation
+ console.log(person.profession);
