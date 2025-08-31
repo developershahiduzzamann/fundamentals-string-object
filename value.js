21a64c4e60dae2a573793881c4bedd09396dc4da
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-08-31 21:28:16 +0600

    add the value js file

---
+++ value.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'sabbir khan',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+    'fev place': ['mirpur', 'kushtia', 'kuakata']
+ }
+ console.log(person,['fev place']);
+ person.salary = 3000;
+ console.log(person);
